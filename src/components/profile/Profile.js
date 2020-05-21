b1a3ec2e8a9c2d66f9ec79ae2dd4f86ce561c9be
Patrick Grady <patickrgrady81@gmail.com>
2020-05-21 17:37:45 -0400

    Added profile pages and set up react routes

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./Profile.css"
+
+export default class Profile extends Component {
+  render = () => {
+    return (
+      <>
+        <h1>This is your profile page. Here you can edit your settings for your exact needs</h1>
+      </>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Profile from './components/profile/Profile';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />,
-  </React.StrictMode>,
+  <Router>
+    <>
+      <Route exact path="/" component={App} />
+      <Route exact path="/weather" component={App} />
+      <Route exact path="/profile" component={Profile} />
+    </>,
+  </Router>,
   document.getElementById('root')
 );
