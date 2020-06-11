107a0048503fc1a0318e31db05b027baac421b5b
Patrick Grady <patickrgrady81@gmail.com>
2020-06-11 14:17:01 -0400

    Users can now register

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Nav from '../../components/nav/Nav';
  
 class Register extends Component {
@@ -21,7 +22,7 @@ class Register extends Component {
         <form onSubmit={this.handleSubmit}>
           <input type="email" name="email" placeholder="email" value={this.state.email} onChange={this.handleChange} required />
           <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.handleChange} required />
-          <input type="password_confirmation" name="password_confirmation" placeholder="confirm password" value={this.state.password}
+          <input type="password" name="password_confirmation" placeholder="confirm password" value={this.state.password_confirmation}
                   onChange={this.handleChange} required />
           <button type="submit">Sign Up</button>
         </form>
@@ -31,21 +32,27 @@ class Register extends Component {
 
   handleSubmit = async (e) => { 
     e.preventDefault();
-    const url = `http://localhost:3001/signup`
+    const url = `http://localhost:3001/api/v1/signup`
     const fetchInfo = {
       "method": "POST",
-      "headers": {},
-      data: {
+      "headers": {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
         user: {
           email: this.state.email,
           password: this.state.password,
           password_confirmation: this.state.password_confirmation
         }
-      }
+      })
     }
     const response = await fetch(url, fetchInfo);
     const data = await response.json();
     console.log(data);
+    if (data.created === true) { 
+      await this.props.login(data.user.email, data.user.id);
+    }
   }
 
   handleChange = (e) => { 
@@ -56,4 +63,16 @@ class Register extends Component {
   }
 }
 
-export default Register;
+const mapStateToProps = state => {
+  return {
+    city: state.city,
+  };
+}
+  
+const mapDispatchToProps = dispatch => {
+  return {
+    login: (email) => dispatch({ type: 'LOGIN',  email })
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
