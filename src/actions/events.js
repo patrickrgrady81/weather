46bcec980ecc296c71794c48cd05e781ac4fd03b
Patrick Grady <patickrgrady81@gmail.com>
2020-10-10 11:23:53 -0400

    change url to be heroku url

--- src/actions/events.js
+++ src/actions/events.js
@@ -9,7 +9,7 @@ export const updateEvents = (city) => {
       },
       body: JSON.stringify({ city })
     }
-    const url = "http://localhost:3001/api/v1/events"
+    const url = "https://pg-city-info.herokuapp.com/api/v1/events"
     const response = await fetch(url, fetchInfo);
     const data = await response.json();
     dispatch({ type: 'LOADING_SUCCESS' });
--- src/actions/restaurants.js
+++ src/actions/restaurants.js
@@ -9,7 +9,7 @@ export const updateRestaurants = (city) => {
       },
       body: JSON.stringify({ city })
     }
-    const url = "http://localhost:3001/api/v1/restaurants"
+    const url = "https://pg-city-info.herokuapp.com/api/v1/restaurants"
     const response = await fetch(url, fetchInfo);
     const data = await response.json();
     dispatch({ type: 'LOADING_SUCCESS' });
